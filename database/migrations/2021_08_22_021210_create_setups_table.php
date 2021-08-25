102544b90823fbd11bbfeb5dcfc7ac3e4d787355
hralamin6 <75069977+hralamin6@users.noreply.github.com>
2021-08-26 01:32:46 +0600

    faisodufad8374

--- database/migrations/2021_08_22_021210_create_setups_table.php
+++ database/migrations/2021_08_22_021210_create_setups_table.php
@@ -16,17 +16,17 @@ class CreateSetupsTable extends Migration
         Schema::create('setups', function (Blueprint $table) {
             $table->id();
             $table->string('status')->default('active');
-            $table->string('logo')->nullable();
+            $table->string('logo')->default('http://lorempixel.com/300/100/nature/6/');
             $table->string('site_name')->nullable()->default('Site name');
-            $table->string('site_url')->nullable()->default('http://hrablog.com');
+            $table->string('site_url')->nullable()->default('https://jpsc.heroquapp.com');
             $table->string('admin')->nullable()->default('Admin');
-            $table->string('email')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('location')->nullable();
+            $table->string('email')->nullable()->default('hralamin2002@gmail.com');
+            $table->string('phone')->nullable()->default('01254789559');
+            $table->string('location')->nullable()->default('dhaka, Bangladesh');
             $table->string('facebook')->nullable()->default('http://facebook.com');
             $table->string('twitter')->nullable()->default('http://twitter.com');
             $table->string('youtube')->nullable()->default('http://youtube.com');
-            $table->text('about')->nullable();
+            $table->text('about')->nullable()->default('asdfa saerawerfasdfadsf');
             $table->timestamps();
         });
     }
